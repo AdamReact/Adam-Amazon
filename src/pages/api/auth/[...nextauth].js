7fd78caac1ef07c92e111c2bc2444a2e6b0032b7
AdamReact <sonnguyenjobb1994@gmail.com>
2022-11-08 20:00:38 +0100

    but why

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,6 +1,5 @@
 import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
-nextauth_secret
 
 export const authOptions = {
   
@@ -10,8 +9,17 @@ export const authOptions = {
     GoogleProvider({
       clientId: process.env.GOOGLE_ID,
       clientSecret: process.env.GOOGLE_SECRET,
+      
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
     }),
     
   ],
+      secret: process.env.NEXTAUTH_SECRET
+  
+
+  
 }
 export default NextAuth(authOptions)
