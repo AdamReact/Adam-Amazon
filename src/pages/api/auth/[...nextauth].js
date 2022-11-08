1144e38cce37096e5e2706078661402d353468a9
AdamReact <sonnguyenjobb1994@gmail.com>
2022-11-08 17:02:21 +0100

    whatever

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,15 +1,12 @@
 import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
+nextauth_secret
 
 export const authOptions = {
   
   
   
   providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_ID,
-      clientSecret: process.env.GOOGLE_SECRET,
-    }),
     GoogleProvider({
       clientId: process.env.GOOGLE_ID,
       clientSecret: process.env.GOOGLE_SECRET,
